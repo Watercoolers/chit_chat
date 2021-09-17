a8000e6cf61481913dee0a8d9b5b4192c73a911a
XavierChanth <xchanthavong@gmail.com>
2021-09-17 00:22:45 -0400

    Update chit_chat to use rsdk

--- lib/services/client_service.dart
+++ lib/services/client_service.dart
@@ -1,8 +1,6 @@
-import 'package:at_chat_flutter/services/chat_service.dart';
 import 'package:at_chat_flutter/utils/init_chat_service.dart';
 import 'package:at_client_mobile/at_client_mobile.dart';
 import 'package:at_client/at_client.dart';
-import 'package:at_commons/at_commons.dart';
 import 'package:at_contacts_flutter/utils/init_contacts_service.dart';
 
 import 'package:path_provider/path_provider.dart';
@@ -21,15 +19,15 @@ class ClientService {
   String? _atsign;
   AtClientPreference? _atClientPreference;
   AtClientService? _atClientServiceInstance;
-  AtClientImpl? _atClientInstance;
+  AtClientManager? _atClientInstance;
 
   //* AtClientService Getters
   AtClientService _getClientServiceForAtsign({String? atsign}) =>
       _atClientServiceInstance ??=
           _atClientServiceMap[atsign] ?? AtClientService();
 
-  AtClientImpl _getClientForAtsign({String? atsign}) => _atClientInstance ??=
-      _getClientServiceForAtsign(atsign: atsign).atClient!;
+  AtClientManager _getClientForAtsign({String? atsign}) => _atClientInstance ??=
+      _getClientServiceForAtsign(atsign: atsign).atClientManager;
 
   //* Onboarding process
   Future<AtClientPreference> getAtClientPreference({String? cramSecret}) async {
@@ -39,7 +37,6 @@ class ClientService {
       ..commitLogPath = path
       ..cramSecret = cramSecret
       ..namespace = AtConstants.NAMESPACE
-      ..syncStrategy = SyncStrategy.IMMEDIATE
       ..rootDomain = AtConstants.ROOT_DOMAIN
       ..rootPort = AtConstants.ROOT_PORT
       ..hiveStoragePath = path;
@@ -52,33 +49,30 @@ class ClientService {
 
   void postOnboard(Map<String?, AtClientService> value, String? atsign) {
     _atClientServiceMap = value;
+    print(_atClientServiceMap);
     _atsign = atsign!;
     var instance = _getClientForAtsign(atsign: _atsign);
+    print(instance.atClient.getCurrentAtSign());
     initializeContactsService(
-      instance,
+      instance.atClient as AtClientImpl,
       atsign,
       rootDomain: AtConstants.ROOT_DOMAIN,
     );
 
     //* This prevents starting multiple monitors for at_chat_flutter
-    if (_clientIsInitialized[atsign] ?? false) {
-      var chatService = ChatService();
-      chatService.currentAtSign = atsign;
-      chatService.atClientInstance = instance;
-    } else {
-      initializeChatService(
-        instance,
-        atsign,
-        rootDomain: AtConstants.ROOT_DOMAIN,
-      );
-      print('initialized chat service for $atsign');
-      _clientIsInitialized[atsign] = true;
-    }
+    initializeChatService(
+      instance,
+      atsign,
+      rootDomain: AtConstants.ROOT_DOMAIN,
+    );
+    print('initialized chat service for $atsign');
+    _clientIsInitialized[atsign] = true;
   }
 
   //* GETTERS (should only be called after onboarding)
   String get atsign => _atsign!;
   AtClientPreference get atClientPreference => _atClientPreference!;
   AtClientService get atClientServiceInstance => _atClientServiceInstance!;
-  AtClientImpl get atClientInstance => _atClientInstance!;
+  AtClientImpl get atClientInstance =>
+      _atClientInstance!.atClient as AtClientImpl;
 }
